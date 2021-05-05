88d607f2c09585ae43132fee73cf606977a67789
AndrewCWheeler <achristopherwheeler@gmail.com>
2021-05-05 16:02:30 -0700

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCZfe6yHTZCUqbSw6yWzfDmysMI9drufeg',
+  authDomain: 'netflix-build-7a2ff.firebaseapp.com',
+  projectId: 'netflix-build-7a2ff',
+  storageBucket: 'netflix-build-7a2ff.appspot.com',
+  messagingSenderId: '44787904180',
+  appId: '1:44787904180:web:1d4a36c7d34575629ab1e1',
+};
