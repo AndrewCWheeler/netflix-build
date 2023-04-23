a290ded28f8c693895997f77d5394ac595862b5f
andrew <andrew@andrews-imac.lan>
2023-04-22 21:12:38 -0700

    edited sign In screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -36,20 +36,23 @@ const SignupScreen = () => {
   };
 
   return (
-    <div className='signupScreen'>
+    <div className="signupScreen">
       <form>
-        <h1>Sign In</h1>
-        <input ref={emailRef} type='email' placeholder='Email' />
-        <input ref={passwordRef} type='password' placeholder='Password' />
-        <button type='submit' onClick={signIn}>
+        <h1>Sign In / Sign Up</h1>
+        <input ref={emailRef} type="email" placeholder="Email" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+        <button type="submit" onClick={signIn}>
           Sign In
         </button>
         <h4>
-          <span className='signupScreen__gray'>New to Netflix? </span>
-          <span className='signupScreen__link' onClick={register}>
-            Sign up now.
+          <span className="signupScreen__gray">
+            New to Netflix? Enter your email and password and click Sign Up
+            instead
           </span>
         </h4>
+        <button type="submit" onClick={register}>
+          Sign Up
+        </button>
       </form>
     </div>
   );
